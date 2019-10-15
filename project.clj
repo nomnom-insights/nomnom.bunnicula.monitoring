ab9a851be5388e9159739b3da097096f65d36c7c
Łukasz Korecki <lukasz@korecki.me>
2019-10-15 21:05:43 +0000

    Release v2.1.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject nomnom/bunnicula.monitoring "2.0.2"
-  :description "Bunnicula: rabbitMQ client"
+(defproject nomnom/bunnicula.monitoring "2.1.0"
+  :description "Monitoring middleware for Bunnicula, with built in support for StatsD metrics and error reporting to Rollbar"
   :url "https://github.com/nomnom-insights/nomnom.bunnicula.monitoring"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.stuartsierra/component "0.3.2"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [nomnom/bunnicula "2.0.2"]
-                 [nomnom/stature "1.0.5"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.stuartsierra/component "0.4.0"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [nomnom/bunnicula "2.1.0"]
+                 [nomnom/stature "2.0.0"]
                  [nomnom/caliban "1.0.2"]]
   :deploy-repositories {"clojars" {:sign-releases false}}
   :min-lein-version "2.5.0"
