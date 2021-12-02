322e7a395cae1e10c66d069127274a3153204e96
Marc Jou <mjou@userzoom.com>
2021-12-02 18:16:00 +0100

    Final version

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject nomnom/bunnicula.monitoring "2.1.0"
+(defproject nomnom/bunnicula.monitoring "2.1.1"
   :description "Monitoring middleware for Bunnicula, with built in support for StatsD metrics and error reporting to Rollbar"
   :url "https://github.com/nomnom-insights/nomnom.bunnicula.monitoring"
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [com.stuartsierra/component "1.0.0"]
                  [org.clojure/tools.logging "1.1.0"]
-                 [nomnom/bunnicula "2.2.3-SNAPSHOT-1"]
-                 [nomnom/stature "2.0.1-SNAPSHOT-1"]
-                 [nomnom/caliban "1.0.3-SNAPSHOT-1"]]
+                 [nomnom/bunnicula "2.2.3"]
+                 [nomnom/stature "2.0.1"]
+                 [nomnom/caliban "1.0.3"]]
   :deploy-repositories {"clojars" {:sign-releases false}}
   :min-lein-version "2.5.0"
   :license {:name "MIT License"
